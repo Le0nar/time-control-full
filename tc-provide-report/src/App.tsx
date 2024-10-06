ee1ea4bb58edb6341d7af48ed939c31293e23eed
Le0nar <yaroslavtrefilov5@gmail.com>
2024-10-06 20:51:18 +0400

    feat(FRONTEND): add auth page

---
+++ tc-provide-report/src/App.tsx
@@ -0,0 +1,24 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import './App.css'
+import { AuthPage } from './pages/auth/Auth.page';
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<AuthPage />} />
+        {/* <Route path="/report" element={<ReportPage />} /> */}
+      </Routes>
+    </Router>
+    <Toaster />
+  </>
+  )
+}
+
+export default App
